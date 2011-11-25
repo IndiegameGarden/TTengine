c8b2e8b99877f6f56e351a55ad66ada6b496cb8d
TranceTrance.com <info@trancetrance.com>
2011-11-25 21:10:39 +0100

    added MsgBox class

---
+++ TTengine/util/MsgBox.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace TTengine.Util
+{
+
+    public class MsgBox
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern int MessageBox(IntPtr hWnd, String text, String caption, uint type);
+
+        /**
+         * Shows a Windows MessageBox with title and content 'msg'
+         */
+        public static void Show(String windowTitle, String msg) 
+        {
+            MessageBox(new IntPtr(0), msg, windowTitle, 0);
+        }
+    }
+}
