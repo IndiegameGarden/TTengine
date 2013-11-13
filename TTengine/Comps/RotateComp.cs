40b64cbce521bd2d221ef6ca0d74de4a12d2a473
TranceTrance <info@trancetrance.com>
2013-11-13 07:51:06 +0100

    merged in changes from Quest repository, unittests-2013-11-13 branch, part 2

---
+++ TTengine/Comps/RotateComp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TTengine.Core;
+
+namespace TTengine.Comps
+{
+    /// <summary>
+    /// Enables rotation of entities
+    /// </summary>
+    public class RotateComp: Comp
+    {
+        public RotateComp()
+        {
+        }
+
+        /// <summary>Rotation angle in radians</summary>
+        public double Rotate = 0;
+    }
+}
