0b5707dd01d4b49e229280ca03cc59dfdb1f501c
TranceTrance <info@trancetrance.com>
2013-12-20 22:24:21 +0100

    Test with 2 'screenlets' working - bg color still wrong

--- TTengineTest/TTengineTest/Tests/TestScreenlets.cs
+++ TTengineTest/TTengineTest/Tests/TestScreenlets.cs
@@ -29,26 +29,27 @@ namespace TTengineTest
 
             // create an additional child channel that renders onto the main channel
             // content for 1st screen: call upon another unit test
-            //var ch1 = TTFactory.CreateChannel(320, 320);
-            //var t1 = new TestRelativeMotion();
-            //t1.Create();
-            //ch.AddChild(ch1);
+            var ch1 = TTFactory.CreateChannel(320, 320);
+            TTFactory.BuildTo(ch1);
+            var t1 = new TestAnimatedSprite();
+            t1.Create();
+            ch.AddChild(ch1);
 
-            /*
             // second child channel
             var ch2 = TTFactory.CreateChannel(400, 320);
-            var t2 = new TestAnimatedSprite();
+            TTFactory.BuildTo(ch2);
+            var t2 = new TestRelativeMotion();
             t2.Create();
             ch.AddChild(ch2);
-            */
 
             // main channel: shows the child channels as sprites
             TTFactory.BuildTo(ch);
-            //var scr1 = TTFactory.CreateSpritelet(ch1);
-            //scr1.GetComponent<DrawComp>().DrawPosition = new Vector2(150f, 50f);
+            var scr1 = TTFactory.CreateSpritelet(ch1);
+            scr1.GetComponent<PositionComp>().Position = new Vector3(350f, 350f,0.1f);
+            scr1.GetComponent<VelocityComp>().Velocity2D = new Vector2(5f, 0.5f);
 
-            //var scr2 = TTFactory.CreateSpritelet(ch2);
-            //scr2.GetComponent<DrawComp>().DrawPosition = new Vector2(540f, 50f);
+            var scr2 = TTFactory.CreateSpritelet(ch2);
+            scr2.GetComponent<PositionComp>().Position = new Vector3(650f, 250f, 0.1f);
         }
 
     }
