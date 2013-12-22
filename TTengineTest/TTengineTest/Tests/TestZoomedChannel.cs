dda9804ebc1088ca30e8113679a468ffc7cb3e09
TranceTrance <info@trancetrance.com>
2013-12-22 15:48:01 +0100

    Added nonblocky graphics to Zoomed channel test

--- TTengineTest/TTengineTest/Tests/TestZoomedChannel.cs
+++ TTengineTest/TTengineTest/Tests/TestZoomedChannel.cs
@@ -23,10 +23,10 @@ namespace TTengineTest
         {
             var ch = TTFactory.BuildChannel; // parent channel
 
-            // dedicated channel for rendering the level
-            var ch1 = TTFactory.CreateChannel(Color.Black,true); 
-            ch1.Screen.SpriteBatch.samplerState = SamplerState.PointClamp; // nice 'n blocky
+            // dedicated channel for rendering the level using blocky (non interpolated) graphics bitmap
+            var ch1 = TTFactory.CreateChannel(Color.Black,true);
             ch.AddChild(ch1);
+            ch1.Screen.SpriteBatch.samplerState = SamplerState.PointClamp; // nice 'n blocky
             TTFactory.BuildTo(ch1);
             var s = TTFactory.CreateSpritelet("Quest14-Level1.png");
             s.GetComponent<SpriteComp>().Center = new Vector2(532f, 227f);
@@ -36,10 +36,11 @@ namespace TTengineTest
 
             // -- main channel: shows the child channel using a sprite
             TTFactory.BuildTo(ch);
-            // some non-blocky graphics in front of level; using default Spritebatch
-            //var t1 = new TestRotation();
-            //t1.Create();                    
             var scr1 = TTFactory.CreateSpritelet(ch1);
+            scr1.GetComponent<PositionComp>().Z = 0.9f;
+            // some non-blocky graphics in front of level; using default Spritebatch
+            var t1 = new TestAnimatedSprite();
+            t1.Create();                    
 
         }
 
