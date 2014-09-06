04fd50fa46d89453e98ae9080a3ecb75da42ea07
IndiegameGarden <info@indiegamegarden.com>
2014-09-06 23:05:40 +0200

    super-duper rework of Effect builder .bat, so that .xnb files are compiled automatically into right location (without requiring the .xnb files in the content project for the copying part)

--- TTengineTest/TTengineTestContent/MonoGame-Effect-build.bat
+++ TTengineTest/TTengineTestContent/MonoGame-Effect-build.bat
@@ -2,9 +2,9 @@
 rem
 rem Build file to build Effect (.fx) using MonoGame content builder tool
 rem
-cd "c:\dev\TT\TTengine-5\TTengineTest\TTengineTestContent\"
+rem cd "c:\dev\TT\TTengine-5\TTengineTest\TTengineTestContent\"
 "C:\Program Files (x86)\MSBuild\MonoGame\v3.0\Tools\MGCB.exe"^
- /outputDir:.^
+ /outputDir:..\TTengineTest\%1Content^
  /intermediateDir:obj^
  /importer:EffectImporter^
  /processor:EffectProcessor^
