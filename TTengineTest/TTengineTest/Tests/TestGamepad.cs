fbcb2aa4e06ad7694bd6f8f04a34a6c03496a751
IndiegameGarden <info@indiegamegarden.com>
2014-09-14 00:02:53 +0200

    Gamepad test complete (needs Xinput type device, not Dinput type)

--- TTengineTest/TTengineTest/Tests/TestGamepad.cs
+++ TTengineTest/TTengineTest/Tests/TestGamepad.cs
@@ -11,11 +11,9 @@ using Artemis.Interface;
 
 namespace TTengineTest
 {
-    /// <summary></summary>
+    /// <summary>Testing the Gamepad (XInput type only) basics</summary>
     class TestGamepad : Test
     {
-        IntPtr joy0;
-
         public TestGamepad()
             : base()        
         {
@@ -26,51 +24,13 @@ namespace TTengineTest
         {
             var b = Factory.CreateMovingBall(TTFactory.BuildScreen.Center, Vector2.Zero);
             TTFactory.AddScript(b, ScriptMoveByGamepad);
-
-            /*
-            int retval = Sdl.SDL_Init(Sdl.SDL_INIT_JOYSTICK | Sdl.SDL_INIT_VIDEO );
-            if (retval != 0)
-            {
-                TTGame.Instance.Exit();
-            }
-            //Sdl.SDL_JoystickEventState(Sdl.SDL_ENABLE);
-            //Sdl.SDL_JoystickEventState(Sdl.SDL_DISABLE);
-            int num = Tao.Sdl.Sdl.SDL_NumJoysticks();
-            joy0 = Sdl.SDL_JoystickOpen(0);
-            if (num == 0)
-            {
-                TTGame.Instance.Exit();
-            }
-             */
-
         }
 
         void ScriptMoveByGamepad(ScriptContext ctx)
         {
             var e = ctx.Entity;
             var vc = e.GetComponent<VelocityComp>();
-            float spd = (float)(95 * ctx.Dt);
-
-            /*
-            byte x = Tao.Sdl.Sdl.SDL_JoystickGetButton(joy0, 255);
-            if (x > 0)
-            {
-                vc.Y -= spd;
-            }
-             */
-
-            /*
-            Tao.Sdl.Sdl.SDL_Event ev = new Tao.Sdl.Sdl.SDL_Event();
-            Sdl.SDL_PollEvent(out ev);
-            if (ev.type == Sdl.SDL_JOYBUTTONDOWN)
-            {
-                vc.Y -= spd;
-            }
-            else if (ev.type != 0)
-            {
-                Sdl.SDL_PushEvent(out ev); // put back
-            }
-            */
+            float spd = (float)(195 * ctx.Dt);
 
             GamePadState gp = GamePad.GetState(PlayerIndex.One);
 
